f4d8e43aa4f532840f2d4f3a258c1fffe1584876
OlliMartin <scholl3ss93@gmail.com>
2025-04-30 08:18:03 +0200

    Cleanup code

--- PiFanCtrl/Services/Temperature/Bmp280TemperatureSensor.cs
+++ PiFanCtrl/Services/Temperature/Bmp280TemperatureSensor.cs
@@ -11,7 +11,7 @@ namespace PiFanCtrl.Services.Temperature;
 
 public sealed class Bmp280TemperatureSensor : ITemperatureSensor, IDisposable
 {
-  private const int DEVICE_OPEN_RETRIES = 50;
+  private const int DEVICE_OPEN_RETRIES = 5;
 
   private readonly ILogger<Bmp280TemperatureSensor> _logger;
   private readonly I2CSensorConfiguration _sensorConfiguration;
@@ -29,10 +29,7 @@ public sealed class Bmp280TemperatureSensor : ITemperatureSensor, IDisposable
     _logger = logger;
     _sensorConfiguration = sensorConfiguration;
 
-    ScanI2C();
-
-    // _sensorConfiguration.I2CAddress ?? Bmp280.DefaultI2cAddress
-    _i2cAddress = 0x76;
+    _i2cAddress = _sensorConfiguration.I2CAddress ?? Bmp280.DefaultI2cAddress;
 
     try
     {
@@ -42,29 +39,6 @@ public sealed class Bmp280TemperatureSensor : ITemperatureSensor, IDisposable
     {
       _logger.LogError(ex, "Cannot open temperature sensor.");
     }
-  }
-
-  private void ScanI2C()
-  {
-    List<int> validAddress = new();
-    Console.WriteLine("Hello I2C!");
-
-// First 8 I2C addresses are reserved, last one is 0x7F
-    for (int i = 8; i < 0x80; i++)
-      try
-      {
-        using I2cDevice i2c = I2cDevice.Create(new(busId: 1, i));
-        byte read = i2c.ReadByte();
-        validAddress.Add(i);
-      }
-      catch (IOException)
-      {
-        // Do nothing, there is just no device
-      }
-
-    _logger.LogInformation("Found {cnt} device(s).", validAddress.Count);
-
-    foreach (int valid in validAddress) _logger.LogInformation("Address: 0x{valid:X}", valid);
   }
 
   private void CreateSensor(I2CSensorConfiguration sensorConfiguration)
