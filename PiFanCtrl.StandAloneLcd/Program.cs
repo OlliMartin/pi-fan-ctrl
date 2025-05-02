5b452c331525697e5f204f9f3e56e23e2f11b947
OlliMartin <scholl3ss93@gmail.com>
2025-05-02 20:09:12 +0200

    Add error handling

--- PiFanCtrl.StandAloneLcd/Program.cs
+++ PiFanCtrl.StandAloneLcd/Program.cs
@@ -4,33 +4,41 @@ using Iot.Device.Graphics;
 using Iot.Device.Graphics.SkiaSharpAdapter;
 using Iot.Device.Ssd13xx;
 
-SkiaSharpAdapter.Register();
+try
+{
+  SkiaSharpAdapter.Register();
 
-Console.WriteLine("Using direct I2C protocol");
+  Console.WriteLine("Using direct I2C protocol");
 
-I2cConnectionSettings connectionSettings = new(busId: 1, deviceAddress: 0x3C);
-using I2cDevice? i2cDevice = I2cDevice.Create(connectionSettings);
-using Ssd1306 device = new(i2cDevice, width: 128, height: 32);
+  I2cConnectionSettings connectionSettings = new(busId: 1, deviceAddress: 0x3C);
+  using I2cDevice? i2cDevice = I2cDevice.Create(connectionSettings);
+  using Ssd1306 device = new(i2cDevice, width: 128, height: 32);
 
-device.ClearScreen();
+  device.ClearScreen();
 
-Console.WriteLine("Display clock");
-int fontSize = 25;
-string font = "DejaVu Sans";
+  Console.WriteLine("Display clock");
+  int fontSize = 25;
+  string font = "DejaVu Sans";
 
-while (!Console.KeyAvailable)
-  using (BitmapImage image = BitmapImage.CreateBitmap(width: 128, height: 32, PixelFormat.Format32bppArgb))
-  {
-    int y = 0;
-    image.Clear(Color.Black);
+  while (!Console.KeyAvailable)
+    using (BitmapImage image = BitmapImage.CreateBitmap(width: 128, height: 32, PixelFormat.Format32bppArgb))
+    {
+      int y = 0;
+      image.Clear(Color.Black);
 
-    IGraphics g = image.GetDrawingApi();
+      IGraphics g = image.GetDrawingApi();
 
-    g.DrawText(DateTime.Now.ToString("HH:mm:ss"), font, fontSize, Color.White, new(x: 0, y));
+      g.DrawText(DateTime.Now.ToString("HH:mm:ss"), font, fontSize, Color.White, new(x: 0, y));
 
-    device.DrawBitmap(image);
+      device.DrawBitmap(image);
 
-    Thread.Sleep(millisecondsTimeout: 100);
-  }
+      Thread.Sleep(millisecondsTimeout: 100);
+    }
 
-Console.ReadKey(intercept: true);
+  Console.ReadKey(intercept: true);
+}
+catch (Exception ex)
+{
+  Console.WriteLine("An error occurred:");
+  Console.WriteLine(ex);
+}
