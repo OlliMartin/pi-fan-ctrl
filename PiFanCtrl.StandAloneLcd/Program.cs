a089fde5f0da232c1a847bb2fb381615edf2983d
OlliMartin <scholl3ss93@gmail.com>
2025-05-02 20:31:26 +0200

    Forgot to register

--- PiFanCtrl.StandAloneLcd/Program.cs
+++ PiFanCtrl.StandAloneLcd/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Iot.Device.Graphics.SkiaSharpAdapter;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PiFanCtrl.StandAloneLcd.Workers;
+
+SkiaSharpAdapter.Register();
 
 IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
 
