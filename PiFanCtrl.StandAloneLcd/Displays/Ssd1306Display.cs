3d7490f4f289ee82b6639c34ce893d4faf8e85e9
OlliMartin <scholl3ss93@gmail.com>
2025-05-04 12:32:47 +0200

    Fixed decimal places

--- PiFanCtrl.StandAloneLcd/Displays/Ssd1306Display.cs
+++ PiFanCtrl.StandAloneLcd/Displays/Ssd1306Display.cs
@@ -38,8 +38,8 @@ public sealed class Ssd1306Display : IDisplay, IDisposable
 
     IGraphics g = image.GetDrawingApi();
 
-    g.DrawText($"Temp: {systemInfo.MeasuredTemperature}", font, fontSize, Color.White, new(x: 0, y: 0));
-    g.DrawText($"Fan%: {systemInfo.PwmPercentage}", font, fontSize, Color.White, new(x: 0, y: 16));
+    g.DrawText($"Temp: {systemInfo.MeasuredTemperature:F2}", font, fontSize, Color.White, new(x: 0, y: 0));
+    g.DrawText($"Fan%: {systemInfo.PwmPercentage:F2}", font, fontSize, Color.White, new(x: 0, y: 16));
 
     Ssd1306 device = GetOrRenewDevice();
     device.DrawBitmap(image);
